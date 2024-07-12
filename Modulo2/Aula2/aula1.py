f92f8c5a52b069041ce4a5725879891cdb811117
Sanderson Luiz Celestino <sandersonluiz1988@gmail.com>
2024-07-12 16:38:19 -0300

    Atribuição de valoresa variaveis

---
+++ Modulo2/Aula2/aula1.py
@@ -0,0 +1,16 @@
+
+# 2020ano = 2020
+
+# print(2020ano)
+
+_2020ano = 2020
+
+print(_2020ano)
+
+# teste espaco = "teste"
+
+# print(teste)
+
+teste_espaco = "teste"
+
+print(teste_espaco)
---
+++ Modulo2/Aula2/exercicio1.py
@@ -0,0 +1,5 @@
+nome = input("Insira seu nome: ")
+idade = input("Insira a sua idade: ")
+email = input("Insira o seu e-mail: ")
+
+print(f"Bom-vindo {nome}, sua idade é {idade} e seu melhor email é {email}")
