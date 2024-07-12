9b7096c46c845c4bec9d4699cb1dc39fadbbb454
Sanderson Luiz Celestino <sandersonluiz1988@gmail.com>
2024-07-12 16:56:52 -0300

    Atribuição de valoresa variaveise tratamentos

---
+++ Modulo2/Aula2/exercicio2.py
@@ -0,0 +1,21 @@
+numero1 = input("Escolha um número: ")
+numero2 = input("Escolha outro número: ")
+
+soma = numero1 + numero2
+
+print(soma)
+print(type(soma))
+
+# Onde está o erro?
+
+numero1 = input("Escolha um número: ")
+numero2 = input("Escolha outro número: ")
+
+# Convertendo as entradas para inteiros
+numero1 = int(numero1)
+numero2 = int(numero2)
+
+soma = numero1 + numero2
+
+print(soma)
+print(type(soma))
