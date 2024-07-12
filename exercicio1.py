079aa707ce955029d78008a8c554bdba941a3449
Sanderson Luiz Celestino <sandersonluiz1988@gmail.com>
2024-07-12 15:42:40 -0300

    exercício  aula1

--- exercicio1.py
+++ exercicio1.py
@@ -2,3 +2,12 @@ print("Sanderson Luiz Celestino")
 
 nome = input("Digite o seu nome: ")
 print(f"Olá {nome}, seja bem-Sandersonvindo a sua primeira aula de Python.")
+
+# Solicitando a cor favorita do usuário
+cor_favorita = input("Qual é a sua cor favorita? ")
+
+# Exibindo a mensagem personalizada
+print(f"Sua cor favorita é {cor_favorita}.")
+
+
+
