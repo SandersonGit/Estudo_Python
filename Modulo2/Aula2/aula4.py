af25a23aae33ccddb9072ddd8c995d0f1ed33fe5
Sanderson Luiz Celestino <sandersonluiz1988@gmail.com>
2024-07-12 17:27:33 -0300

    Operadores de comparação

---
+++ Modulo2/Aula2/aula4.py
@@ -0,0 +1,49 @@
+# Operadores relacionais
+
+# Operadores == e !=
+
+a = 10
+b = 10
+c = 15
+
+print(a == b)
+print(b == c)
+
+d = 10
+e = 5
+f = 10
+
+print(d != e)
+print(d != f)
+
+idade = 18
+censura = 18
+
+print(idade == censura)
+
+print("------------------------------------------------------------")
+# Operador > e <
+
+a = 10
+b = 5
+c = 20
+
+print(a > b)
+print( a > c )
+
+print(a < b)
+print(a < c)
+
+print("------------------------------------------------------------")
+
+# Operador >= ou <=
+
+a = 15
+b = 10
+c = 15
+
+print(a >= b)
+print(a >= c)
+
+print(a <= b)
+print(a <= c)
