175ba3b159fa03f34dc632a223346b74d9c69fad
Sanderson Luiz Celestino <sandersonluiz1988@gmail.com>
2024-07-12 18:43:05 -0300

    Expresões Lógicas

---
+++ Modulo2/Aula2/aula6.py
@@ -0,0 +1,14 @@
+# Expressões Lógicas
+
+idade = 18
+carteira = True
+
+print(idade >= 18 and carteira == True)
+print("Pode dirigir")
+
+velociade = 90
+radar = 100
+radarFuncionando = False
+
+print(velociade >= radar and radarFuncionando == True)
+print("Não foi multado")
---
+++ Modulo2/Aula2/exercicio6.py
@@ -0,0 +1,30 @@
+# portugues = float(input("Nota de Portugues: "))
+# matematica = float(input("Nota de Matemática: "))
+# geografia = float(input("Nota de Geografia: "))
+
+# total = portugues + matematica + geografia
+
+# media = total / 3
+
+# print(media, f"Se a sua média for igual ou maior a 7 vc passou, se não vc reprovou.") 
+
+# print("--------------------------------------------------------------------")
+
+salario = int(1500)
+aumento = int(250)
+
+novoSalario = salario + aumento
+
+print(novoSalario)
+
+print("--------------------------------------------------------------------")
+
+carroA = 100
+carroB = 80
+carroC = 90
+
+limite = 90
+
+print(carroA > limite)
+print(carroB > limite)
+print(carroC > limite)
