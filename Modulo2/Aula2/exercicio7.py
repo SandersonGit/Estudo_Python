64f0743ef5102178631cc3784daae23180eadd53
Sanderson Luiz Celestino <sandersonluiz1988@gmail.com>
2024-07-12 19:05:30 -0300

    Variaveis com string

---
+++ Modulo2/Aula2/exercicio7.py
@@ -0,0 +1,18 @@
+saudacao = "Olá"
+nome = "Sanderson"
+
+print(f"{saudacao}, {nome}!")
+print(nome[0])
+print(nome[3])
+print(nome[8])
+print(len(nome))
+
+nome = input("Insira seu nome:")
+saudacao = input("Uma saudação:")
+
+print(f"{saudacao}, {nome}")
+print(f"Primeiro caractere é {nome[0]}")
+print(f"Terceiro caractere é {nome[2]}")
+print(f"Ultimo caractere é {nome[-1]}")
+print(len(nome))
+
