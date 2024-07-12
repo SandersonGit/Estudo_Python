602f8ad922d0ac840ad2115ccaf8f1f52398d734
Sanderson Luiz Celestino <sandersonluiz1988@gmail.com>
2024-07-12 20:15:05 -0300

    Variaveis com string

--- Modulo2/Aula2/exercicio7.py
+++ Modulo2/Aula2/exercicio7.py
@@ -1,18 +1,32 @@
-saudacao = "Olá"
-nome = "Sanderson"
+# saudacao = "Olá"
+# nome = "Sanderson"
 
-print(f"{saudacao}, {nome}!")
-print(nome[0])
-print(nome[3])
-print(nome[8])
-print(len(nome))
+# print(f"{saudacao}, {nome}!")
+# print(nome[0])
+# print(nome[3])
+# print(nome[8])
+# print(len(nome))
 
-nome = input("Insira seu nome:")
-saudacao = input("Uma saudação:")
+# nome = input("Insira seu nome:")
+# saudacao = input("Uma saudação:")
 
-print(f"{saudacao}, {nome}")
-print(f"Primeiro caractere é {nome[0]}")
-print(f"Terceiro caractere é {nome[2]}")
-print(f"Ultimo caractere é {nome[-1]}")
-print(len(nome))
+# print(f"{saudacao}, {nome}")
+# print(f"Primeiro caractere é {nome[0]}")
+# print(f"Terceiro caractere é {nome[2]}")
+# print(f"Ultimo caractere é {nome[-1]}")
+# print(len(nome))
+
+# poupanca = float(1245.86)
+
+# print(poupanca)
+
+saldo = 359.56
+print(saldo)
+deposito = saldo + float(input("Digite seu depósito"))
+saldo = deposito
+fatura = 125.98
+saldo = saldo - fatura
+print(f"Seu valor na conta é de R${saldo}")
+
+
 
