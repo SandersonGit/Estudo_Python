b894b5cce30d0d755b812803850ce89b7618accb
Sanderson Luiz Celestino <sandersonluiz1988@gmail.com>
2024-07-12 16:15:29 -0300

    Atribuição de valores

---
+++ Modulo2/exercio1.py
@@ -0,0 +1,9 @@
+amigo1 = input("Seu melhor amigo?")
+amigo2 = input("Seu segundo melhor amigo?")
+amigo3 = input("Seu terceiro melhor amigo?")
+
+type_amigo1 = type(amigo1)
+type_amigo2 = type(amigo2)
+type_amigo3 = type(amigo3)
+
+print(f"Seus melhores amigos são: {amigo1 , type_amigo1}, {amigo2, type_amigo2} e {amigo3, type_amigo3}")
