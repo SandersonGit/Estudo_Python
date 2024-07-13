6076406e0ca4f804afff6eee9214d25f83234ae0
Sanderson Luiz Celestino <sandersonluiz1988@gmail.com>
2024-07-13 18:20:30 -0300

    loop for

---
+++ Modulo4 repetição/for.py
@@ -0,0 +1,13 @@
+palavra = "teste"
+
+for letra in palavra:
+    print(letra)
+    
+nome = "Sanderson Luiz Celestino"
+
+for letra in nome:
+    print(letra)
+    if letra == "S":
+        print("Letra inicial")
+        
+
---
+++ Modulo4 repetição/while.py
@@ -0,0 +1,50 @@
+# print("Antes do loop")
+
+# numero = 0
+
+# while numero < 5:
+#     print(numero)
+#     numero = numero + 1
+
+# print("Depois do loop")
+
+# # Exercício
+
+# numero = 10
+
+# while numero <= 50:
+#     print(numero)
+#     numero = numero + 1
+
+
+# numero = 20
+
+# while numero >= 0:
+#     print(numero)
+#     numero = numero -1
+
+
+# limite = int(input("Insira um número para ser o limite de while"))
+
+# numero = 0
+
+# while numero <= limite:
+#     print(numero)
+#     numero = numero + 1
+
+# numero = 0
+
+# while numero <= 50:
+#     result = numero % 2
+#     if result == 0:
+#         print(f"{numero} é par")
+#     numero = numero + 1
+
+
+numero = 0
+
+while numero <= 0:
+    numero = int(input("Dite outro numero"))
+    print(numero)
+    if numero == 0:
+        break
