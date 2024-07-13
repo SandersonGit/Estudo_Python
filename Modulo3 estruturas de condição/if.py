d6c8cc8e6bd3bccc03c53d8cdd477772d61617cd
Sanderson Luiz Celestino <sandersonluiz1988@gmail.com>
2024-07-13 15:18:15 -0300

    Condicionais

---
+++ Modulo3 estruturas de condição/if.py
@@ -0,0 +1,57 @@
+# if 10> 5:
+#     legumes = "cebola"
+#     print("entrou no if")
+#     print("cenoura", legumes)
+
+# print("antes do if")
+
+# if 5 > 10:
+#     print("If falso")
+
+# print("Continuação")
+
+# nome = "Sanderson" 
+
+# if nome == "Sanderson":
+#     print(f"Olá {nome}!")
+    
+# idade = int(input("Qual a sua idade?"))
+
+# if idade >= 18:
+#     print("Pode entrar na balada")
+# else:
+#     print("Só maiores")
+    
+# veiculo = int(input("Quantos rodas possui o veiculo?"))
+
+# if veiculo > 2:
+#     print("Paga pedágio")
+# else:
+#     print("Pode passar")
+
+# n1 = int(input("Me diga um número"))
+# n2 = int(input("Outo número"))
+
+# if n1 > n2:
+#     print(f"O primeiro número {n1}, é maior que o segundo número {n2}")
+# else:
+#     print(f"O segundo número {n2}, é maior que o primeiro número {n1}.")
+
+# salario = int(input("Qual o seu salário?"))
+
+# if salario >= 1800:
+#     print("Você precisa pagar imposto de renda")
+# else:
+#     print("Você não precisa pagar imposto de renda")
+       
+n1 = int(input("Insira um número"))
+n2 = int(input("Insira outro número"))
+
+print(n1, n2)
+
+multiplicacao = n1 * n2
+
+if multiplicacao >= 100:
+    print(f"Os dois número multiplicados no valor de {multiplicacao} é acima de 100")
+else:
+    print(f"Os dois número multiplicados no valor de {multiplicacao} é abaixo de 100")
