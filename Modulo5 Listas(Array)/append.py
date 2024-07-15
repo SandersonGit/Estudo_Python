790a6a7b3af6fd7307c83f53e6ec78820abc9b6d
Sanderson Luiz Celestino <sandersonluiz1988@gmail.com>
2024-07-15 15:34:37 -0300

    listas

--- Modulo5 Listas(Array)/append.py
+++ Modulo5 Listas(Array)/append.py
@@ -22,18 +22,44 @@
     
 # print(lista)
 
-frutas = ["Maçã", "Uva", "Cajú"]
-verduras = ["Alface", "Couve", "Agrião"]
-juntos = []
+# frutas = ["Maçã", "Uva", "Cajú"]
+# verduras = ["Alface", "Couve", "Agrião"]
+# juntos = []
+
+# i = 0
+# while i < len(frutas):
+#     juntos.append(frutas[i])
+#     i += 1
+
+# i = 0
+# while i < len(verduras):
+#     juntos.append(verduras[i])
+#     i += 1
+
+# print(juntos)
+
+
+lista1 = [15, 23, 12, 48, 53, 98, 42]
+lista2 = [2, 8, 64, 53, 48, 65, 72, 33, 30]
+combinadosPares = []
+
 
 i = 0
-while i < len(frutas):
-    juntos.append(frutas[i])
+
+while i < len(lista1):
+    if lista1[i] % 2 ==  0:
+        combinadosPares.append(lista1[i])
+
     i += 1
+
+print(combinadosPares)
 
 i = 0
-while i < len(verduras):
-    juntos.append(verduras[i])
+
+while i < len(lista2):
+    if lista2[i] % 2 == 0:
+        combinadosPares.append(lista2[i])
+
     i += 1
 
-print(juntos)
+print(combinadosPares)
