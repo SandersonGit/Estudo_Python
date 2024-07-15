18f139c55d397102622f6c76226e590249bea507
Sanderson Luiz Celestino <sandersonluiz1988@gmail.com>
2024-07-15 14:00:08 -0300

    listas

---
+++ Modulo5 Listas(Array)/exercicios.py
@@ -0,0 +1,9 @@
+nomes = ["", "", "", "", ""]
+i = 0
+
+while i < 5:
+    nome = str(input("Digite um nome"))
+    nomes[i] = nome
+    i = i + 1
+
+print(nomes)
---
+++ Modulo5 Listas(Array)/listas.py
@@ -0,0 +1,25 @@
+lista = []
+print(lista)
+print(type(lista))
+
+numeros = [1, 2, 3, 4, 5]
+
+print(numeros)
+
+nomes = ["Sanderson", "Melanie", "Heitor", "Maria"]
+
+print(nomes)
+
+nomes[1] = "Cecilia"
+
+print(nomes)
+
+notas = [10, 8, 5, 10]
+
+contador = 0
+
+# while contador < 4:
+#     result = result + notas[contador]
+#     contador = contador + 1
+#     media = result / 4
+#     print(media)
