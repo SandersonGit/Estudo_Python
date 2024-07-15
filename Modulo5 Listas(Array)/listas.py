5d70451746702f0550296fc7f0ca5741543f5526
Sanderson Luiz Celestino <sandersonluiz1988@gmail.com>
2024-07-15 14:18:02 -0300

    listas

--- Modulo5 Listas(Array)/listas.py
+++ Modulo5 Listas(Array)/listas.py
@@ -1,25 +1,49 @@
-lista = []
-print(lista)
-print(type(lista))
+# lista = []
+# print(lista)
+# print(type(lista))
 
-numeros = [1, 2, 3, 4, 5]
+# numeros = [1, 2, 3, 4, 5]
 
-print(numeros)
+# print(numeros)
 
-nomes = ["Sanderson", "Melanie", "Heitor", "Maria"]
+# nomes = ["Sanderson", "Melanie", "Heitor", "Maria"]
 
-print(nomes)
+# print(nomes)
 
-nomes[1] = "Cecilia"
+# nomes[1] = "Cecilia"
 
-print(nomes)
+# print(nomes)
 
-notas = [10, 8, 5, 10]
+# notas = [10, 8, 5, 10]
 
-contador = 0
+# contador = 0
 
 # while contador < 4:
 #     result = result + notas[contador]
 #     contador = contador + 1
 #     media = result / 4
 #     print(media)
+
+# lista1 = [1, 2, 3, 4]
+
+# lista2 = lista1[:]
+
+# lista2[1] = 5
+
+# print(lista1, lista2)
+
+# slice em python
+
+lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+novaLista = lista[0:5]
+
+print(novaLista)
+
+novaLista2 = lista[7:]
+
+print(novaLista2)
+
+novaLista3 = lista[:6]
+
+print(novaLista3)
