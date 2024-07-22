3d7c1be5220ba7555a8f1e77bbdd62d2cee31deb
Sanderson Luiz Celestino <sandersonluiz1988@gmail.com>
2024-07-22 19:01:57 -0300

    Funções

--- Funções/3_retorno_de_funcao/app.py
+++ Funções/3_retorno_de_funcao/app.py
@@ -6,3 +6,11 @@ def saudacao(nome):
 sds = saudacao("Sanderson")
 
 print(sds)
+
+#  Define a função imprimeNome que imprime uma saudação com um nome. O nome é um parâmetro opcional com valor padrão "Matheus". Se nenhum nome for fornecido, a função usa o valor padrão. A função é chamada duas vezes: uma sem argumentos, usando o nome padrão, e outra com o nome "Melanie".
+
+def imprimeNome(nome = "Matheus"):
+    print(f"Olá, {nome}!")
+
+imprimeNome()
+imprimeNome("Melanie")
