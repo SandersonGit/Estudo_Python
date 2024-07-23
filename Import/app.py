77b2bce356cc54107558a057b61b5cbcad944c8c
Sanderson Luiz Celestino <sandersonluiz1988@gmail.com>
2024-07-23 11:09:21 -0300

    funções

---
+++ Import/app.py
@@ -0,0 +1,17 @@
+
+
+import funcao
+
+
+funcao.olaModulo()
+
+a = funcao.soma(5,5)
+print(a)
+b = funcao.subtracao(10,5)
+print(b)
+c = funcao.multiplicacao(10,2)
+print(c)
+d = funcao.divisao(20,2)
+print(d)
+
+
---
+++ Import/funcao.py
@@ -0,0 +1,24 @@
+def olaModulo():
+    print("Olá Módulo")
+    
+def soma(num1, num2):
+    soma = num1 + num2
+   
+    return soma
+
+def subtracao(num1, num2):
+    subtracao = num1 - num2
+        
+    return subtracao
+
+def multiplicacao(num1, num2):
+    multiplicacao = num1 * num2
+    
+    return multiplicacao
+
+def divisao(num1, num2):
+    divisao = num1 / num2
+    
+    return divisao
+    
+    
